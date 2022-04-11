238d816cdd3177966cce393f33d8b75d609152fe
Haedong Kim <haedong1111@gmail.com>
2022-04-11 18:30:56 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 'use strict';
 
-// Make navbar transparent when it is on the top
+// Navbar
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
+
+// Make navbar transparent when it is on the top
 document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// Handle scrolling when clicking on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+  const id = event.target.dataset.section;
+  if (!id) return;
+  const target = document.getElementById(id);
+  const targetY = target.getBoundingClientRect().y;
+  const distance = window.pageYOffset + targetY - navbarHeight;
+  window.scroll({
+    top: distance,
+    left: 0,
+    behavior: 'smooth',
+  });
 });
